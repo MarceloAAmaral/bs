ceaf7f39b5e53400ba64b464f4ea26b56304760b
MarceloAAmaral <marceloamaralfpoli@gmail.com>
2022-10-24 20:50:40 -0300

    primeiro commit

---
+++ html/menu.php
@@ -0,0 +1,13 @@
+<header id="header">
+<div id="logo">
+            <a href="<?PHP echo $url; ?>"><img src="<?PHP echo $url; ?>img/logo_bsrun.png" alt="BSRUN Assessoria Esportiva" title="BSRUN Assessoria Esportiva"/></a>
+        </div>
+    <div class='flex alg-items'>
+        <span data-icon='menu' class="bt-menu item">MENU</span>    
+		
+    </div>
+        
+        <div class="cadastro_login item">CADASTRAR/LOGIN</div>
+    
+</header>
+<?PHP echo menu(); ?>
---
+++ url.php
@@ -0,0 +1,14 @@
+<?PHP
+$dados = array();
+$protocolo = "http://";
+$dominio= $_SERVER['HTTP_HOST'];
+$cliente = "bs";
+$request=$_SERVER['REQUEST_URI'];
+
+$dados['url']['request'] = $request;
+$dados['url']['dominio'] = $dominio;
+$dados['url']['inicio'] = $protocolo.$dominio.'/'.$cliente;
+$dados['url']['href'] = $dados['url']['inicio'];
+$dados['post'] = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+$get = filter_input_array(INPUT_GET, FILTER_DEFAULT);
+
